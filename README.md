124bc949d2dda3b31d4941ddcd94044bd8908f6d
fuchunhui <fuchuhu7@gmail.com>
2021-08-17 18:57:13 +0800

    feat: server test

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ auto image robot.
 way1: by text, send message.
 
 way2: by http, get img.
+
+### roadmap
+
+1. node 服务接收请求参数。
+2. 
---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+const app = express();
+
+// parse application/json
+app.use(express.json());
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+const hostName = '127.0.0.1';
+const port = 8080;
+
+app.all('*', (req, res, next) => {  
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', '3.2.1');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
+app.get('/get',(req, res) => {
+  console.log('请求url：', req.path);
+  console.log('请求参数：', req.query);
+  res.send('这是get请求');
+})
+
+app.post('/post', (req, res) => {
+  console.log('请求参数：', req.body, JSON.stringify(req.body));
+  const result = {code: 200, msg: 'post请求成功'};
+  res.send(result);
+});
+
+app.listen(port, hostName, () => {
+  console.log(`服务器运行在 http://${hostName}:${port}`);
+});
