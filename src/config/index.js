872a3a08cb9d30d184f906b74ddedfa859b48abd
fuchunhui <fuchuhu7@gmail.com>
2021-08-24 17:37:14 +0800

    feat: parser

--- src/config/index.js
+++ src/config/index.js
@@ -6,9 +6,10 @@ if (env === 'test') {
   process.env.MEME_BASE_URL = 'http://localhost:8080';
 }
 
-const {server, traget} = config; // TODO 更新替换
+const {server, traget, key} = config; // TODO 更新替换
 
 export default {
   server,
-  traget
+  traget,
+  key
 };
---
+++ src/convert/parser.js
@@ -0,0 +1,67 @@
+import crypto from 'crypto-js';
+import config from '../config/index.js';
+
+const {enc, mode, pad, AES} = crypto;
+const {key} = config;
+
+class AESCipher {
+  constructor(key) {
+    this.key = enc.Base64.parse(key);
+    this.options = {
+      mode: mode.ECB,
+      padding: pad.Pkcs7
+    };
+  }
+
+  encrypt(data) {
+    const cipher = AES.encrypt(data, this.key, this.options);
+    const base64Cipher = cipher.ciphertext.toString(enc.Base64);
+    const result = base64Cipher
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=+$/g, '');
+    return result;
+  }
+
+  decrypt(data) {
+    const content = data
+      .replace(/-/g, '+')
+      .replace(/_/g, '/')
+      .padEnd(data.length + data.length % 4, '=');
+    const bytes = AES.decrypt(content, this.key, this.options);
+    return bytes.toString(enc.Utf8);
+  }
+}
+
+const _decode = encryption => {
+  const cipher = new AESCipher(key);
+  const data = cipher.decrypt(encryption);
+  console.log('消息体内容', data); // TODO delete
+  return JSON.parse(data);
+};
+
+const parser = encryption => {
+  const content = _decode(encryption);
+  const {header, body} = content.message;
+  const {fromuserid: fromid, toid} = header;
+  const cell = body.find(item => item.type === 'TEXT');
+  const message =  cell ? cell.content.trim() : '';
+  let command = '';
+  let text = '';
+  if (message) {
+    const info = message.split(' ');
+    command = info[0];
+    text = info.length > 1 ? info[1] : '';
+  }
+
+  return {
+    fromid,
+    toid,
+    command,
+    text
+  };
+};
+
+export {
+  parser
+};
