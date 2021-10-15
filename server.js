4a2f1a98bae6cbdf9f7345423df30e2da0bcc6b2
fuchunhui <fuchuhu7@gmail.com>
2021-10-15 12:10:22 +0800

    fix: request too large, add limit.

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ import config from './src/config/index.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(express.text());
 app.use(express.raw());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'public')));
