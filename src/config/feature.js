f7484b2d004f034ed1d8bfa4f46c33fdb491ad49
fuchunhui <fuchuhu7@gmail.com>
2021-11-29 20:28:57 +0800

    feat: update config

--- src/config/feature.js
+++ src/config/feature.js
@@ -1,1 +1,1 @@
-export default [{"feature":"周报","type":"TEXT","x":10,"y":10,"width":100,"height":100}];
+export default [{"feature":"周报","type":"TEXT","x":10,"y":10,"width":100,"height":100,"sid":""}];
