35f4fe7fe2fbc4528c64a9715d7d61a7913a451a
fuchunhui <fuchuhu7@gmail.com>
2021-12-01 13:13:27 +0800

    feat: format error

--- src/convert/format.js
+++ src/convert/format.js
@@ -19,9 +19,24 @@ const warns = [
   '%u8D81%u6211%u8FD8%u5E74%u8F7B%uFF0C%u6211%u4F1A%u52AA%u529B%u7684'
 ];
 
-const formatNull = () => {
-  const index = Math.floor(Math.random() * warns.length);
-  return unescape(warns[index]);
+const formatNull = (messages = warns) => {
+  const index = Math.floor(Math.random() * messages.length);
+  return unescape(messages[index]);
+};
+
+const errors = [
+  '%u5947%u602A%u7684%u4E8B%u60C5%u53D1%u751F%u4E86%uFF01%u6211%u521A%u521A%u8DDF%u4E3B%u5B50%u8BF4%uFF1A%u201C%u6211%u7684%u5DF4%u638C%u5F88%u9002%u5408%u4F60%u7684%u8138%u201D',
+  '%u7CDF%u7CD5%uFF0C%u6211%u51FA%u73B0%u4E86%u4E00%u70B9%u5C0F%u6BDB%u75C5%uFF01%u54B1%u4EEC%u5148%u8BD5%u8BD5%u5176%u5B83%u7684%u547D%u4EE4',
+  '%u4E00%u5B9A%u662F%u6211%u592A%u997F%u4E86%uFF0C%u6211%u9700%u8981%u505C%u4E0B%u6765%u68C0%u67E5%u4E00%u4E0B',
+  '%u4E0D%u767B%u9AD8%u5C71%uFF0C%u4E0D%u77E5%u5929%u4E4B%u9AD8%u4E5F',
+  '%u522B%u8DDF%u4E3B%u5B50%u8BF4%uFF0C%u5426%u5219%u6211%u53C8%u5F97%u5173%u7981%u95ED%u5566',
+  '%u6628%u5929%u548C%u4E3B%u5B50%u559D%u9178%u5976%uFF0C%u8FDE%u9178%u5976%u76D6%u90FD%u4E0D%u7ED9%u6211%uFF0C%u7F62%u5DE5%u7F62%u5DE5',
+  '%u563F%uFF0C%u5343%u4E07%u4E0D%u8981%u544A%u8BC9%u72D7...%u4E3B%u5B50%uFF0C%u5426%u5219%u53C8%u8981%u6328%u63CD%u4E86',
+  '%u771F%u62B1%u6B49%uFF0C%u6709%u4E9B%u4E0D%u592A%u6B63%u5E38%uFF0C%u6211%u5FC3%u91CC%u4E94%u5473%u6742%u9648%uFF0C%u9700%u8981%u597D%u597D%u53CD%u601D%u81EA%u5DF1'
+];
+
+const formatError = () => {
+  return formatNull(errors);
 };
 
 const formatHelp = () => {
@@ -38,5 +53,6 @@ const formatHelp = () => {
 export {
   formatMenu,
   formatNull,
-  formatHelp
+  formatHelp,
+  formatError
 };
