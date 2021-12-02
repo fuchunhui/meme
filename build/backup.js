dd4b91edda4d5048ceef8e5cf79fea77f4db921f
fuchunhui <fuchuhu7@gmail.com>
2021-12-03 01:03:51 +0800

    feat: update db

--- build/backup.js
+++ build/backup.js
@@ -3,7 +3,9 @@ import fs from 'fs';
 import {backup} from '../app.js';
 
 const date = new Date();
-const datePath = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+const month = (date.getMonth() + 1).toString().padStart(2, '0');
+const day = date.getDate().toString().padStart(2, '0');
+const datePath = `${date.getFullYear()}-${month}-${day}`;
 
 const PUBLIC = 'assets/backup';
 const __dirname = path.resolve();
