f9c7067694615fb913862ebcc138fe6ffbf3035d
fuchunhui <fuchuhu7@gmail.com>
2021-12-11 15:21:23 +0800

    docs: add use doc

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,7 @@
 /**
  * 检测文本中是否包含带有双引号包含的空格串，如【abc de "fg hijk"】
  * @param {*} value 检测值
- * @returns 
+ * @returns 匹配后的内容或者 null
  */
 const matchText = value => {
   const pattern = /“.*\s+.*”|".*\s+.*"$/g;
